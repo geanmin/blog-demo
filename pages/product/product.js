f804ab59cc33a7da789bfc1e236f072bf4256e82
geanmin <2277123921@qq.com>
2020-01-03 13:53:01 +0800

    商品详情

--- pages/product/product.js
+++ pages/product/product.js
@@ -1,4 +1,6 @@
-import {Product} from 'product-model.js'
+import {
+  Product
+} from 'product-model.js'
 
 var product = new Product();
 
@@ -8,27 +10,36 @@ Page({
    * 页面的初始数据
    */
   data: {
-    background:[],
-    productDets:[],
+    loadingHidden:false,
+    background: [],
+    productDets: [],
+    num:1,//商品数量
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
     this._onLoadData();
+    //先从内存中获取商品数量
   },
 
   /**
    * 商品详情页
    */
-  _onLoadData:function(callback){
-      product.getProductDetsInfo((data)=>{
-        console.log(data);
-          this.setData({
-            productDets:data,
-            background:data.imgArr,
-          })
-      });
+  _onLoadData: function(callback) {
+    product.getProductDetsInfo((data) => {
+      this.setData({
+        loadingHidden: true,
+        productDets: data,
+        background:data.imgArr,
+        
+      })
+    });
+  },
+
+  /**商品数量减少 */
+  productreduce: function(event){
+    console.log(event);
   }
 })
