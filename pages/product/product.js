e3e79df6aec533c8afebb890bd9adf286ff5998d
geanmin <2277123921@qq.com>
2020-01-03 16:21:12 +0800

    login

--- pages/product/product.js
+++ pages/product/product.js
@@ -10,10 +10,11 @@ Page({
    * 页面的初始数据
    */
   data: {
-    loadingHidden:false,
+    loadingHidden: false,
     background: [],
     productDets: [],
-    num:1,//商品数量
+    num: 1, //商品数量
+    cartTotalCounts: 0, //购物车数量
   },
 
   /**
@@ -32,14 +33,52 @@ Page({
       this.setData({
         loadingHidden: true,
         productDets: data,
-        background:data.imgArr,
-        
+        background: data.imgArr,
+
       })
     });
   },
 
   /**商品数量减少 */
-  productreduce: function(event){
+  productreduce: function(event) {
     console.log(event);
+  },
+
+  /**加入购物车 */
+  onAddingToCartTap: function(events) {
+    // console.log(event);
+    this._flyToCartEffect(events);
+  },
+  /**添加购物车动效 */
+  _flyToCartEffect: function(events) {
+    var touches = events.touches[0];
+    // console.log(events);
+    var diff = {
+        X: 145 + touches.clientX + 'px',
+        Y: '571px',
+      },
+      style = 'display: block;-webkit-transform:translate(' + diff.x + ',' + diff.y + ') rotate(350deg) scale(0)'; //移动距离
+    this.setData({
+      isFly: true,
+      translateStyle: style
+    });
+    var that = this;
+    setTimeout(() => {
+      that.setData({
+        isFly: false,
+        translateStyle: '-webkit-transform: none;', //恢复到最初状态
+        isShake: true,
+      });
+      setTimeout(() => {
+        var counts = that.data.cartTotalCounts + that.data.productCounts;
+        that.setData({
+          isShake: false,
+          cartTotalCounts: counts
+        });
+      }, 200);
+    }, 1000);
+  },
+  onclick: function(events) {
+    console.log(events);
   }
 })
