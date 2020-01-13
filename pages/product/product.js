75a90b5a97a4ded1ce5425ed9db8f0ab9d04bb54
geanmin <2277123921@qq.com>
2020-01-13 17:42:48 +0800

    商品详情页开始

--- pages/product/product.js
+++ pages/product/product.js
@@ -1,5 +1,9 @@
-import { Product} from 'product-model.js'
-import {Common} from '../../utils/common.js'
+import {
+  Product
+} from 'product-model.js'
+import {
+  Common
+} from '../../utils/common.js'
 
 var product = new Product();
 var common = new Common();
@@ -44,8 +48,8 @@ Page({
     var num = common.inputVailNum(num);
     //改变焦点时，覆盖掉商品数量
     var that = this;
-    if((num - that.data.cartTotalCounts)<0){
-       return;
+    if ((num - that.data.cartTotalCounts) < 0) {
+      return;
     }
     that.setData({
       productCounts: num,
@@ -54,11 +58,11 @@ Page({
   /**商品数量减少 */
   productreduce: function(event) {
     //同时传递是加减
-    this._flyToCartEffect(event,0);
+    this._flyToCartEffect(event, 0);
   },
 
   /**商品数量添加 */
-  productadd:function(event){
+  productadd: function(event) {
     //同时传递是加减
     this._flyToCartEffect(event, 1);
   },
@@ -68,11 +72,27 @@ Page({
   onAddingToCartTap: function(event) {
     //同时传递是加减
     this._flyToCartEffect(event, 1);
+    //同时将购物车数据加入到内存去
+    this.addToCart();
   },
 
+  /**购物车加入内存中 */
+  addToCart: function() {
+    var tmpObj = {};
+    var keys = ['id', 'name', 'price', 'img'];
+
+      //遍历放到对象中
+      for (var key in this.data.productDets) {
+        if (keys.indexOf(key) >= 0) {
+          tmpObj[key] = this.data.product[key];
+        }
+      }
+    //加入到购物车
+    card.add(tmpObj, this.data.productDets);
+  },
 
   /**添加购物车动效 */
-  _flyToCartEffect: function(events,type) {
+  _flyToCartEffect: function(events, type) {
     var touches = events.touches[0];
     var diff = {
         X: 240 + touches.clientX + 'px',
@@ -91,12 +111,12 @@ Page({
         isShake: true,
       });
       setTimeout(() => {
-        if(type == 0){
+        if (type == 0) {
           var counts = that.data.cartTotalCounts - that.data.productCounts;
-        }else{
+        } else {
           var counts = that.data.cartTotalCounts + that.data.productCounts;
         }
-        
+
         that.setData({
           isShake: false,
           cartTotalCounts: counts,
@@ -105,6 +125,6 @@ Page({
     }, 1000);
   },
   onclick: function(events) {
-    
+
   }
 })
